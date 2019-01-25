2bcbd2d091824a0a859b5b891b77c1d2ed3a0d08
Ariel Fisher <fishondor@gmail.com>
2019-01-25 14:28:20 +0200

    Set video ad page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { VideoAdComponent } from './pages/video-ad/video-ad.component';
+
+const routes: Routes = [
+  { path: 'video-add', component: VideoAdComponent },
+  { path: '', component: VideoAdComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
