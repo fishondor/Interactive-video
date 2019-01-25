539ea13026f5c5280b25c7a94d3d2a16dbae8947
Ariel Fisher <fishondor@gmail.com>
2019-01-25 14:29:05 +0200

    Remove initial content and app module set for added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DeviceDetectorModule } from 'ngx-device-detector';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoAdComponent } from './pages/video-ad/video-ad.component';
+import { ActionButtonComponent } from './components/action-button/action-button.component';
+import { DynamicActionComponentComponent } from './components/dynamic-action-component/dynamic-action-component.component';
+import { IconButtonComponent } from './components/icon-button/icon-button.component';
+import { TrackingPixelComponent } from './components/tracking-pixel/tracking-pixel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoAdComponent,
+    ActionButtonComponent,
+    DynamicActionComponentComponent,
+    IconButtonComponent,
+    TrackingPixelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DeviceDetectorModule.forRoot()
   ],
   providers: [],
+  entryComponents: [
+    ActionButtonComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
